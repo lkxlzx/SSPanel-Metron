1d965e89a0ee1619c21f8424ef1a5beeaef516cc
lkxlzx <40789172+lkxlzx@users.noreply.github.com>
2024-11-18 21:15:16 +0800

    Update LinkController.php
    修复订阅trojan节点重复的问题

--- src/Controllers/LinkController.php
+++ src/Controllers/LinkController.php
@@ -978,8 +978,6 @@ class LinkController extends BaseController
                 $return_url .= URL::get_NewAllUrl($user, $Rule);
                 $Rule['type'] = 'ss';
                 $return_url .= URL::get_NewAllUrl($user, $Rule);
-                $Rule['type'] = 'trojan';
-                $return_url .= URL::get_NewAllUrl($user, $Rule);
                 $getListExtend = $Rule['extend'] ? self::getListExtend($user, 'v2rayn') : [];
                 break;
             case 4: // Trojan
